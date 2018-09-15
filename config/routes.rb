80a77200bc6d89cf9ed61e13b9e84189ad1cfc32
Zarina <sharipzar@gmail.com>
2018-09-16 00:00:10 +0300

    Added home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resource :home, only: [:index, :show]
+  root to: 'home#index'
+
+  #def resource(action, options={})
+
+  #end
 end
