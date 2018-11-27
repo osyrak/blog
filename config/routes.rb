cc93a14f73b358e93b29e40cdaf5d2daa45012dd
Zarina <sharipzar@gmail.com>
2018-11-27 21:36:41 +0300

    config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resource :home, only: [:index, :show]
-  root to: 'home#index'
+  resource :home, only: [:index]
+  root to: 'posts#index'
 
   resources :posts, only: [:index]
 
